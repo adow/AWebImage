c7ec21c21fc32db9c8b8ee84870f20ab7bd64b53
adow <reynoldqin@gmail.com>
2016-06-01 22:47:16 +0800

    Initial Commit

---
+++ AWebImage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AWebImage
+//
+//  Created by 秦 道平 on 16/6/1.
+//  Copyright © 2016年 秦 道平. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
